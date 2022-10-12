ccc2016be88c849012a6b2ab728f8ce5097312a2
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-12 16:03:37 +0200

    feat: solve dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,7 +16,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function countCats(matrix) {
   //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
   let cnt_cats = 0;
   for (let i = 0; i < matrix.length; i++)
     for (let j = 0; j < matrix[0].length; j++) {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(members)) return false;
+  //members = members.filter(member => typeof member === 'string' && member);
+  //members = members.map(member => member.trim()[0].toUpperCase());
+  //members.sort(function cmp(a, b) { return a<b ? -1 : 1; });
+  //let res = members.join('');
+  //return res ? res : false;
+  return members.filter(member => typeof member === 'string' && member).map(member => member.trim()[0].toUpperCase()).sort(function cmp(a, b) { return a<b? -1:1; }).join('') ;
 }
 
 module.exports = {
