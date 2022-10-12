2eedbeaf120418a191bd66c175108d1979578b97
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-12 21:33:41 +0200

    feat: solve encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  //throw new NotImplementedError('Not implemented');
+  let tmp = 1;
+  let res = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i+1]) { tmp++; }
+    else { 
+      if (tmp > 1) res.push(tmp+str[i]); 
+      else res.push(str[i]);
+        tmp = 1; 
+    }
+  }
+  return res.join('');
 }
 
 module.exports = {
