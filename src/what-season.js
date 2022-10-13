08c3895e5ed108a4766bd6eb6af4877a0d6f5767
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-13 23:58:04 +0200

    feat: solve mac-address

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
+  if (typeof date == 'undefined') { return 'Unable to determine the time of year!'; }
+  if (isNaN(Date.parse(date))) { throw Error('Invalid date!'); }
+  //if (typeof(date) != 'object') { throw Error('Invalid date!'); }
+  let month = date.getMonth();
+  if (isNaN(month)) { throw Error('Invalid date!'); }
+  if (month >= 0 && month <= 1 || month === 11) { return 'winter'; }
+  if (month >= 2 && month <= 4) { return 'spring'; }
+  if (month >= 5 && month <= 7) { return 'summer'; }
+  if (month >= 8 && month <= 10) { return 'autumn'; }
+
 }
 
 module.exports = {
