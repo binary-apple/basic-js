b4bfd53308a5759e851bc65f9509de0766d8b78f
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-13 13:11:08 +0200

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
+  if (Number(sampleActivity) == NaN || sampleActivity === undefined || sampleActivity === null) return false;
+  let n = Number(sampleActivity);
+  if (typeof sampleActivity != 'string' || isNaN(n) || n <= 0 || n > MODERN_ACTIVITY) return false;
+  return(Math.ceil(Math.log(MODERN_ACTIVITY/n)*HALF_LIFE_PERIOD/0.693));
 }
 
 module.exports = {
