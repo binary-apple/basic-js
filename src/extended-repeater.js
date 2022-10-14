3a04eae83aa1623533e5ef54408c788b03d32cf0
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-15 00:55:11 +0200

    feat: refactor extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,26 +17,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function repeater(str, options) {
   //throw new NotImplementedError('Not implemented');
-  let tmpStrRep = '';
-  if (typeof str != 'string') str = String(str);
+  let tmpStrRep = new Array(options.additionRepeatTimes || 1)
+    .fill('addition' in options ? String(options.addition) : '')
+    .join(options.additionSeparator || '|');
 
-  if (!('additionRepeatTimes' in options) && 'addition' in options) { tmpStrRep = options.addition; }
-  if ('addition' in options && 'additionRepeatTimes' in options) {
-    if (typeof options.addition != 'string') options.addition = String(options.addition);
-    if (!('additionSeparator' in options)) { 
-      options.additionSeparator = '|';
-    } else { options.additionSeparator = String(options.additionSeparator); }
-
-    tmpStrRep = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
-  }
-  if ('repeatTimes' in options) {
-    if (!('separator' in options)) { 
-      options.separator = '+';
-    } else { options.separator = String(options.separator); }
-
-    tmpStrRep = new Array(options.repeatTimes).fill(str+tmpStrRep).join(options.separator);
-  } else return str+options.addition;
-  return tmpStrRep;
+  return new Array(options.repeatTimes || 1)
+    .fill(String(str) + tmpStrRep)
+    .join(options.separator || '+');
 }
 
 module.exports = {
