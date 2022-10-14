a88e6673b709a79bf0b0b4ae4267c79064cad705
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-14 23:25:14 +0200

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,17 +17,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function repeater(str, options) {
   //throw new NotImplementedError('Not implemented');
+  let tmpStrRep = '';
   if (typeof str != 'string') str = String(str);
-  if (typeof options.addition != 'string') options.addition = String(options.addition);
-  if (!Boolean(options.separator)) options.separator = '+';
-  if (!Boolean(options.additionSeparator)) options.additionSeparator = '|';
-  
-  
-  let add = new Array(options.additionRepeatTimes);
-  add.fill(options.addition);
-  let strNew = new Array(options.repeatTimes);
-  strNew.fill(str + add.join(options.additionSeparator));
-  return strNew.join(add.join(options.separator));
+
+  if (!('additionRepeatTimes' in options) && 'addition' in options) { tmpStrRep = options.addition; }
+  if ('addition' in options && 'additionRepeatTimes' in options) {
+    if (typeof options.addition != 'string') options.addition = String(options.addition);
+    if (!('additionSeparator' in options)) { 
+      options.additionSeparator = '|';
+    } else { options.additionSeparator = String(options.additionSeparator); }
+
+    tmpStrRep = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+  }
+  if ('repeatTimes' in options) {
+    if (!('separator' in options)) { 
+      options.separator = '+';
+    } else { options.separator = String(options.separator); }
+
+    tmpStrRep = new Array(options.repeatTimes).fill(str+tmpStrRep).join(options.separator);
+  } else return str+options.addition;
+  return tmpStrRep;
 }
 
 module.exports = {
