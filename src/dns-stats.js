b7e524697d9d7f1df38f1cbdcdb8654dbbbe6b25
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-16 21:46:34 +0200

    feat: solve dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  //throw new NotImplementedError('Not implemented');
+  let tmpStr = '';
+  let dotPos = 0;
+  let obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    domains[i] = '.' + domains[i].split('.').reverse().join('.');
+  }
+  for (let i = 0; i < domains.length; i++) {
+    while(1) {
+      tmpStr = domains[i].slice(0, (domains[i].indexOf('.', dotPos+1) == -1) ? domains[i].length : domains[i].indexOf('.', dotPos+1));
+      dotPos = tmpStr.length;
+      if (tmpStr in obj) { obj[tmpStr]++; }
+      else { obj[tmpStr] = 1; }
+      if (tmpStr == domains[i]) {
+        dotPos = 0;
+        break;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = {
