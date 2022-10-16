eeb74c8e2efaa8dfcd5fdd31925251eeb4cdc8ef
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-17 01:33:00 +0200

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isReverse = true) {
+    this.isReverse = isReverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
+    if (arguments.length != 2 || message === undefined || key === undefined) { throw Error('Incorrect arguments!'); }
+
+    let encryptedMessage = [];
+    let i = 0, j = 0;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    while (i < message.length) {
+      if (!(new RegExp('[a-zA-Z]')).test(message[i])) {
+        encryptedMessage.push(message[i]);
+        i++;
+        continue;
+      }
+      encryptedMessage.push(String.fromCharCode(
+        (key[j % key.length].charCodeAt()
+        - 2 * 'A'.charCodeAt()
+        + message[i].charCodeAt()) % 26
+        + 'A'.charCodeAt()
+        ));
+      i++; j++;
+    }
+
+    return this.isReverse ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+
+  }
+  decrypt(encryptedMessage, key) {
+    //throw new NotImplementedError('Not implemented');
+    if (arguments.length != 2 || encryptedMessage === undefined || key === undefined) { throw Error('Incorrect arguments!'); }
+
+    let message = [];
+    let i = 0, j = 0;
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    while (i < encryptedMessage.length) {
+      if (!(new RegExp('[a-zA-Z]')).test(encryptedMessage[i])) {
+        message.push(encryptedMessage[i]);
+        i++;
+        continue;
+      }
+       message.push(String.fromCharCode(
+         (encryptedMessage[i].charCodeAt()
+         - key[j % key.length].charCodeAt()
+         + 26) % 26
+         + 'A'.charCodeAt()
+         ));
+      i++; j++;
+    }
+
+    return this.isReverse ? message.join('') : message.reverse().join('');
+
   }
 }
 
